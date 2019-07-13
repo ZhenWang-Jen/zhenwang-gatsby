67706196ecff8404d71a8c8ee197abf7e9aeafce
Jen Wang <jenwang@Jens-MacBook-Air.local>
2019-07-12 21:08:33 -0700

    First Commit

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Jen', // <title>
+  manifestName: 'Jen',
+  manifestShortName: 'Portfolio', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+
+  authorName: 'Jen Wang',
+  authorHeading: 'Web Developer/Innovator/Doer',
+  authorSocialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/ZhenWang-Jen',
+    },
+    {
+      icon: 'fa-wechat',
+      name: 'Wechat',
+      url:
+        'https://o96tf.sse.codesandbox.io/static/wechat-e653d7e622312e6a5e86824d2c62b29e.jpg',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:zwang303@asu.edu',
+    },
+  ],
+};
